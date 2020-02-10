d582d318270c22e5cba67b3fd5755428481ae9f9
mengyao <36824962+mengyao-liu@users.noreply.github.com>
2020-02-10 21:51:55 +0800

    Update TensorFlow_Tutorial.py

--- TensorFlow_Tutorial.py
+++ TensorFlow_Tutorial.py
@@ -8,3 +8,330 @@ from tf_utils import load_dataset, random_mini_batches, convert_to_one_hot, pred
 
 %matplotlib inline
 np.random.seed(1)
+
+# the loss of one training example
+y_hat = tf.constant(36, name='y_hat')            # Define y_hat constant. Set to 36.
+y = tf.constant(39, name='y')                    # Define y. Set to 39
+
+loss = tf.Variable((y - y_hat)**2, name='loss')  # Create a variable for the loss
+
+init = tf.global_variables_initializer()         # When init is run later (session.run(init)),
+                                                 # the loss variable will be initialized and ready to be computed
+with tf.Session() as session:                    # Create a session and print the output
+    session.run(init)                            # Initializes the variables
+    print(session.run(loss))                     # Prints the loss
+    
+    
+a = tf.constant(2)
+b = tf.constant(10)
+c = tf.multiply(a,b)
+print(c)
+sess = tf.Session()
+print(sess.run(c))
+
+# placeholder
+x = tf.placeholder(tf.int64, name = 'x')
+print(sess.run(2 * x, feed_dict = {x: 3}))
+sess.close()
+
+
+def linear_function():
+    """
+    Implements a linear function: 
+            Initializes X to be a random tensor of shape (3,1)
+            Initializes W to be a random tensor of shape (4,3)
+            Initializes b to be a random tensor of shape (4,1)
+    Returns: 
+    result -- runs the session for Y = WX + b 
+    """
+    
+    np.random.seed(1)
+    
+    """
+    Note, to ensure that the "random" numbers generated match the expected results,
+    please create the variables in the order given in the starting code below.
+    (Do not re-arrange the order).
+    """
+    X = tf.constant(np.random.randn(3,1), name = "X")
+    W = tf.constant(np.random.randn(4,3), name = "W")
+    b = tf.constant(np.random.randn(4,1), name = "b")
+    Y = tf.add(tf.matmul(W, X), b)
+    
+    # Create the session using tf.Session() and run it with sess.run(...) on the variable you want to calculate
+    
+    sess = tf.Session()
+    result = sess.run(Y)
+    
+    # close the session 
+    sess.close()
+
+    return result
+  
+  
+def sigmoid(z):
+    """
+    Computes the sigmoid of z
+    
+    Arguments:
+    z -- input value, scalar or vector
+    
+    Returns: 
+    results -- the sigmoid of z
+    """
+    
+    # Create a placeholder for x. Name it 'x'.
+    x = tf.placeholder(tf.float32, name = "x")
+
+    # compute sigmoid(x)
+    sigmoid = tf.sigmoid(x)
+
+    # Create a session, and run it. Please use the method 2 explained above. 
+    # You should use a feed_dict to pass z's value to x. 
+    with tf.Session() as sess: 
+        # Run session and call the output "result"
+        result = sess.run(sigmoid, feed_dict = {x:z})
+
+    
+    return result
+
+  
+def cost(logits, labels):
+    """
+    Computes the cost using the sigmoid cross entropy
+    
+    Arguments:
+    logits -- vector containing z, output of the last linear unit (before the final sigmoid activation)
+    labels -- vector of labels y (1 or 0) 
+    
+    Note: What we've been calling "z" and "y" in this class are respectively called "logits" and "labels" 
+    in the TensorFlow documentation. So logits will feed into z, and labels into y. 
+    
+    Returns:
+    cost -- runs the session of the cost (formula (2))
+    """
+    
+    
+    # Create the placeholders for "logits" (z) and "labels" (y) (approx. 2 lines)
+    z = tf.placeholder(tf.float32, name = "z")
+    y = tf.placeholder(tf.float32, name = "y")
+    
+    # Use the loss function (approx. 1 line)
+    cost = tf.nn.sigmoid_cross_entropy_with_logits(logits = z,  labels = y)
+    
+    # Create a session (approx. 1 line). See method 1 above.
+    sess = tf.Session()
+    
+    # Run the session (approx. 1 line).
+    cost = sess.run(cost, feed_dict = {z:logits,y:labels})
+    
+    # Close the session (approx. 1 line). See method 1 above.
+    sess.close()
+    
+    
+    return cost
+  
+  
+def one_hot_matrix(labels, C):
+    """
+    Creates a matrix where the i-th row corresponds to the ith class number and the jth column
+                     corresponds to the jth training example. So if example j had a label i. Then entry (i,j) 
+                     will be 1. 
+                     
+    Arguments:
+    labels -- vector containing the labels 
+    C -- number of classes, the depth of the one hot dimension
+    
+    Returns: 
+    one_hot -- one hot matrix
+    """
+    
+    
+    # Create a tf.constant equal to C (depth), name it 'C'. (approx. 1 line)
+    C = tf.constant(C)
+    
+    # Use tf.one_hot, be careful with the axis (approx. 1 line)
+    one_hot_matrix = tf.one_hot(labels,C,axis=0)
+    
+    # Create the session (approx. 1 line)
+    sess = tf.Session()
+    
+    # Run the session (approx. 1 line)
+    one_hot = sess.run(one_hot_matrix)
+    
+    # Close the session (approx. 1 line). See method 1 above.
+    sess.close()
+    
+    
+def ones(shape):
+    """
+    Creates an array of ones of dimension shape
+    
+    Arguments:
+    shape -- shape of the array you want to create
+        
+    Returns: 
+    ones -- array containing only ones
+    """
+    
+    ### START CODE HERE ###
+    
+    # Create "ones" tensor using tf.ones(...). (approx. 1 line)
+    ones = tf.ones(shape)
+    
+    # Create the session (approx. 1 line)
+    sess = tf.Session()
+    
+    # Run the session to compute 'ones' (approx. 1 line)
+    ones = sess.run(ones)
+    
+    # Close the session (approx. 1 line). See method 1 above.
+    sess.close()
+    
+    ### END CODE HERE ###
+    return ones
+    
+    
+    return one_hot
+  
+  
+  
+###########################  Problem statement: SIGNS Dataset  ###########################
+
+def create_placeholders(n_x, n_y):
+    """
+    Creates the placeholders for the tensorflow session.
+    
+    Arguments:
+    n_x -- scalar, size of an image vector (num_px * num_px = 64 * 64 * 3 = 12288)
+    n_y -- scalar, number of classes (from 0 to 5, so -> 6)
+    
+    Returns:
+    X -- placeholder for the data input, of shape [n_x, None] and dtype "tf.float32"
+    Y -- placeholder for the input labels, of shape [n_y, None] and dtype "tf.float32"
+    
+    Tips:
+    - You will use None because it let's us be flexible on the number of examples you will for the placeholders.
+      In fact, the number of examples during test/train is different.
+    """
+
+    X = tf.placeholder(tf.float32, [n_x,None],name = "Placeholder_1")
+    Y = tf.placeholder(tf.float32, [n_y,None],name = "Placeholder_2")
+    
+    return X, Y
+  
+  
+def initialize_parameters():
+    """
+    Initializes parameters to build a neural network with tensorflow. The shapes are:
+                        W1 : [25, 12288]
+                        b1 : [25, 1]
+                        W2 : [12, 25]
+                        b2 : [12, 1]
+                        W3 : [6, 12]
+                        b3 : [6, 1]
+    
+    Returns:
+    parameters -- a dictionary of tensors containing W1, b1, W2, b2, W3, b3
+    """
+    
+    tf.set_random_seed(1)                   # so that your "random" numbers match ours
+        
+    W1 = tf.get_variable("W1", [25,12288], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    b1 = tf.get_variable("b1", [25,1], initializer = tf.zeros_initializer())
+    W2 = tf.get_variable("W2", [12,25], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    b2 = tf.get_variable("b2", [12,1], initializer = tf.zeros_initializer())
+    W3 = tf.get_variable("W3", [6,12], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    b3 = tf.get_variable("b3", [6,1], initializer = tf.zeros_initializer())
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2,
+                  "W3": W3,
+                  "b3": b3}
+    
+    return parameters
+  
+
+def forward_propagation(X, parameters):
+    """
+    Implements the forward propagation for the model: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SOFTMAX
+    
+    Arguments:
+    X -- input dataset placeholder, of shape (input size, number of examples)
+    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3"
+                  the shapes are given in initialize_parameters
+
+    Returns:
+    Z3 -- the output of the last LINEAR unit
+    """
+    
+    # Retrieve the parameters from the dictionary "parameters" 
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    W3 = parameters['W3']
+    b3 = parameters['b3']
+    
+    ### START CODE HERE ### (approx. 5 lines)              # Numpy Equivalents:
+    Z1 = tf.add(tf.matmul(W1, X), b1)                      # Z1 = np.dot(W1, X) + b1
+    A1 = tf.nn.relu(Z1)                                    # A1 = relu(Z1)
+    Z2 = tf.add(tf.matmul(W2, A1), b2)                     # Z2 = np.dot(W2, A1) + b2
+    A2 = tf.nn.relu(Z2)                                    # A2 = relu(Z2)
+    Z3 = tf.add(tf.matmul(W3, A2), b3)                     # Z3 = np.dot(W3, A2) + b3
+    ### END CODE HERE ###
+    
+    return Z3
+  
+  
+def compute_cost(Z3, Y):
+    """
+    Computes the cost
+    
+    Arguments:
+    Z3 -- output of forward propagation (output of the last LINEAR unit), of shape (6, number of examples)
+    Y -- "true" labels vector placeholder, same shape as Z3
+    
+    Returns:
+    cost - Tensor of the cost function
+    """
+    
+    # to fit the tensorflow requirement for tf.nn.softmax_cross_entropy_with_logits(...,...)
+    logits = tf.transpose(Z3)
+    labels = tf.transpose(Y)
+    
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+    
+    return cost
+  
+
+
+# Loading the dataset
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+# Example of a picture
+index = 0
+plt.imshow(X_train_orig[index])
+print ("y = " + str(np.squeeze(Y_train_orig[:, index])))
+
+# Flatten the training and test images
+X_train_flatten = X_train_orig.reshape(X_train_orig.shape[0], -1).T
+X_test_flatten = X_test_orig.reshape(X_test_orig.shape[0], -1).T
+# Normalize image vectors
+X_train = X_train_flatten/255.
+X_test = X_test_flatten/255.
+# Convert training and test labels to one hot matrices
+Y_train = convert_to_one_hot(Y_train_orig, 6)
+Y_test = convert_to_one_hot(Y_test_orig, 6)
+
+print ("number of training examples = " + str(X_train.shape[1]))
+print ("number of test examples = " + str(X_test.shape[1]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+  
+  
+  
+    
