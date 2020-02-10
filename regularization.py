89204a0ad02c8a9c6731937e16f281766f6bd388
mengyao <36824962+mengyao-liu@users.noreply.github.com>
2020-02-10 14:05:22 +0800

    Create regularization.py

---
+++ regularization.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from reg_utils import sigmoid, relu, plot_decision_boundary, initialize_parameters, load_2D_dataset, predict_dec
+from reg_utils import compute_cost, predict, forward_propagation, backward_propagation, update_parameters
+import sklearn
+import sklearn.datasets
+import scipy.io
+from testCases import *
+
+%matplotlib inline
+plt.rcParams['figure.figsize'] = (7.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
