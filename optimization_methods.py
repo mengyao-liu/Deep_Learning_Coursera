a494c9365b150efbca2bc8ca5996b6417f66175a
mengyao <36824962+mengyao-liu@users.noreply.github.com>
2020-02-10 17:52:01 +0800

    Create optimization_methods.py

---
+++ optimization_methods.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+import math
+import sklearn
+import sklearn.datasets
+
+from opt_utils_v1a import load_params_and_grads, initialize_parameters, forward_propagation, backward_propagation
+from opt_utils_v1a import compute_cost, predict, predict_dec, plot_decision_boundary, load_dataset
+from testCases import *
+
+%matplotlib inline
+plt.rcParams['figure.figsize'] = (7.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
